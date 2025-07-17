9b9851f070187d9a6300788f8f79ccc77e845137
rahul6364 <rkulal6177@gmail.com>
2025-07-17 00:49:36 -0600

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,5 +1,9 @@
 def call(String url,String branch){
   sh "echo this is clonning the code"
-  git url: url , branch: branch
-  sh "clonning is completed"
+  try{
+      git url: url , branch: branch
+      sh "clonning is completed"
+  }catch(e){
+    sh "echo there is errore in clonning of ${url} is  ${e.message()}"
+  }
 }
